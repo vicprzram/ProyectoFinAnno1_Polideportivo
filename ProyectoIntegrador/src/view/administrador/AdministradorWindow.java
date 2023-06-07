a88284f0c73bbd7f1e0c2815a1a0771c000a2cd4
daniel <daniel>
2023-06-07 17:26:02 +0200

    panel administrador

---
+++ ProyectoIntegrador/src/view/administrador/AdministradorWindow.java
@@ -0,0 +1,83 @@
+package view.administrador;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+import control.EmpleadoListener;
+
+import javax.swing.ScrollPaneConstants;
+import java.awt.BorderLayout;
+
+public class AdministradorWindow extends JFrame{
+	private JMenuBar menuBar;
+	private JMenuItem mntmGestionEmple, mntmGestionInsta, mntmRegistro, mntmCerrar, mntmInicio;
+	
+	public static final String ITEM_EMPLEADOS = "Gestion empleados";
+	public static final String ITEM_INSTALACIONES = "Gestion instalaciones";
+	
+	public static final String ITEM_SESION = "Cerrar sesion";
+	public static final String ITEM_INICIO = "Inicio";
+	
+	private static final int ANCHO = 800;
+	private static final int ALTO = 600;
+	
+	private JScrollPane scrpContenedor;
+	
+	public AdministradorWindow() {
+		super("Administardos");
+		init();
+		
+	}
+	private void init() {
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		scrpContenedor = new JScrollPane();
+		getContentPane().add(scrpContenedor);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mntmInicio = new JMenuItem(ITEM_INICIO);
+		mntmInicio.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(mntmInicio);
+		mntmGestionEmple = new JMenuItem(ITEM_EMPLEADOS);
+		mntmGestionEmple.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(mntmGestionEmple);
+		
+		mntmGestionInsta = new JMenuItem(ITEM_INSTALACIONES);
+		mntmGestionInsta.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(mntmGestionInsta);
+		
+		mntmCerrar = new JMenuItem(ITEM_SESION);
+		mntmCerrar.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(mntmCerrar);
+		
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		centrarVentana();
+	}
+	private void centrarVentana() {
+		setSize(ANCHO, ALTO);
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension ventana = new Dimension(ANCHO, ALTO);
+		setLocation((pantalla.width-ventana.width)/2,(pantalla.height-ventana.height)/2);
+	}
+	public void cargarPanel(JPanel panel) {
+		scrpContenedor.setViewportView(panel);
+	}
+	public void setListener(EmpleadoListener l) {
+		this.mntmInicio.addActionListener(l);
+		this.mntmCerrar.addActionListener(l);
+		this.mntmGestionEmple.addActionListener(l);
+		this.mntmGestionInsta.addActionListener(l);
+	
+	}
+
+}
